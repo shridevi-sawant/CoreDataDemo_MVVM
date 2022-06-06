9964859dc204afcbc3f2b3b5f354cdc029926eea
shridevi-sawant <shridevi.hiroji@gmail.com>
2022-06-06 15:28:14 +0530

    test

--- CoreDataDemo/View/ViewController.swift
+++ CoreDataDemo/View/ViewController.swift
@@ -20,7 +20,7 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     
-        
+        print("ssss")
         empVM.getEmp()
         
         print("Total emps: \(empVM.empList.count)")
